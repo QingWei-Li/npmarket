78ec6a5a6d5000731be0dccfab7c442ba76a01ff
qingwei.li <cinwell.li@gmail.com>
2018-06-12 16:16:34 +0800

    add logo

--- src/components/readme-panel.vue
+++ src/components/readme-panel.vue
@@ -9,70 +9,75 @@
     <div
       class="ReadmePanel__empty"
       v-else>
-      <h2>npmarket</h2>
+      <img src="~@/assets/logo.png" alt="npmarket" class="logo">
       <p>More efficient search for node packages</p>
-      <p class="author">Created by <a href="//github.com/qingwei-li">QingWei-Li</a></p>
+      <p class="author">Created by <a href="//github.com/qingwei-li">QingWei-Li</a> & <a href="//github.com/mirzazulfan">Mirza Zulfan</a></p>
     </div>
   </div>
 </template>
 
 <script>
-  import markdown from '@/utils/markdown'
+import markdown from '@/utils/markdown';
 
-  export default {
-    name: 'ReadmePanel',
+export default {
+  name: 'ReadmePanel',
 
-    props: ['content'],
+  props: ['content'],
 
-    data: () => ({
-      scrollTo: 0
-    }),
+  data: () => ({
+    scrollTo: 0
+  }),
 
-    computed: {
-      html() {
-        this.scrollTo = 0
+  computed: {
+    html() {
+      this.scrollTo = 0;
 
-        return markdown(this.content)
-      }
+      return markdown(this.content);
     }
   }
+};
 </script>
 
 <style src="prismjs/themes/prism.css"></style>
 <style src="github-markdown-css/github-markdown.css"></style>
 <style>
-  .ReadmePanel {
-    padding: 45px;
-    box-sizing: border-box;
+.ReadmePanel {
+  padding: 45px;
+  box-sizing: border-box;
 
-    &__empty {
-      text-align: center;
-      align-items: center;
-      margin-top: 16vh;
+  &__empty {
+    text-align: center;
+    align-items: center;
+    margin-top: 16vh;
 
-      h2 {
-        font-size: 50px;
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        color: #0084ff;
-        margin-bottom: 20px;
-      }
+    h2 {
+      font-size: 50px;
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      color: #0084ff;
+      margin-bottom: 20px;
+    }
 
-      p {
-        font-size: 20px;
-        margin-bottom: 20px;
-      }
+    .logo {
+      width: 256px;
+      margin-bottom: 30px;
+    }
 
-      .author {
-        font-size: 14px;
-      }
+    p {
+      font-size: 20px;
+      margin-bottom: 20px;
+    }
+
+    .author {
+      font-size: 14px;
     }
   }
+}
 
-  iframe {
-    outline: none;
-    border: none;
-    display: block;
-    width: 100%;
-    height: 306px;
-  }
+iframe {
+  outline: none;
+  border: none;
+  display: block;
+  width: 100%;
+  height: 306px;
+}
 </style>
